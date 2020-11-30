b65f9201ca5b291fce2ee596642ba2b3b7a0fd29
Uddip Mitra <uddipmitra@gmail.com>
2020-11-30 14:21:41 -0500

    Fix location fields

--- src/Models/CampModels.cs
+++ src/Models/CampModels.cs
@@ -18,14 +18,14 @@ namespace CoreCodeCamp.Models
         [Range(1,100)]
         public int Length { get; set; } = 1;
 
-        public string VenueName { get; set; }
-        public string Address1 { get; set; }
-        public string Address2 { get; set; }
-        public string Address3 { get; set; }
-        public string CityTown { get; set; }
-        public string StateProvince { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
+        public string Venue { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
 
         public ICollection<TalkModel> Talks { get; set; }
     }
