fd69e45bd28244d09451d3ea92429cbf622269a7
Uddip Mitra <uddipmitra@gmail.com>
2020-11-30 14:21:06 -0500

    Fixed profile for automapper

--- src/Data/CampProfile.cs
+++ src/Data/CampProfile.cs
@@ -13,17 +13,13 @@ namespace CoreCodeCamp.Data
         public CampProfile()
         {
             this.CreateMap<Camp, CampModel>()
-                .ForMember(c => c.VenueName, o => o.MapFrom(m => m.Location.VenueName))
-                .ForMember(c => c.Address1, o => o.MapFrom(m => m.Location.Address1))
-                .ForMember(c => c.Address2, o => o.MapFrom(m => m.Location.Address2))
-                .ForMember(c => c.Address3, o => o.MapFrom(m => m.Location.Address3))
-                .ForMember(c => c.CityTown, o => o.MapFrom(m => m.Location.CityTown))
-                .ForMember(c => c.StateProvince, o => o.MapFrom(m => m.Location.StateProvince))
-                .ForMember(c => c.PostalCode, o => o.MapFrom(m => m.Location.PostalCode))
-                .ForMember(c => c.Country, o => o.MapFrom(m => m.Location.Country));
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName)).ReverseMap();
             this.CreateMap<Talk, TalkModel>();
+            this.CreateMap<Talk, TalkModel>().ReverseMap();
             this.CreateMap<Speaker, SpeakerModel>();
-            this.CreateMap<CampModel, Camp>();
+            this.CreateMap<Speaker, SpeakerModel>().ReverseMap();
         }
 
     }
