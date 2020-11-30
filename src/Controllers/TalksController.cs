7f3a2ac510babfc78d18c312b9738a08b0d927f7
Uddip Mitra <uddipmitra@gmail.com>
2020-11-30 15:19:46 -0500

    Implement Get Talks by moniker and Postman request

---
+++ src/Controllers/TalksController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/camps/{moniker}/talks")]
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository repository;
+        private readonly IMapper mapper;
+        private readonly LinkGenerator linkGenerator;
+
+        public TalksController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this.repository = repository;
+            this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> Get(string moniker)
+        {
+            try
+            {
+                var talks = await this.repository.GetTalksByMonikerAsync(moniker);
+                return mapper.Map<TalkModel[]>(talks);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to get Talks");
+            }
+        }
+
+        private ActionResult<TalkModel[]> StatusCode(object statuscodes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
