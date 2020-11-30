5836e8a26f51eca5b04ea09f68faf69eab8677d4
Uddip Mitra <uddipmitra@gmail.com>
2020-11-29 23:58:47 -0500

    Use automapper ForMember

--- src/Models/CampModels.cs
+++ src/Models/CampModels.cs
@@ -13,13 +13,13 @@ namespace CoreCodeCamp.Models
         public DateTime EventDate { get; set; } = DateTime.MinValue;
         public int Length { get; set; } = 1;
 
-        public string LocationVenueName { get; set; }
-        public string LocationAddress1 { get; set; }
-        public string LocationAddress2 { get; set; }
-        public string LocationAddress3 { get; set; }
-        public string LocationCityTown { get; set; }
-        public string LocationStateProvince { get; set; }
-        public string LocationPostalCode { get; set; }
-        public string LocationCountry { get; set; }
+        public string VenueName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string CityTown { get; set; }
+        public string StateProvince { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
     }
 }
