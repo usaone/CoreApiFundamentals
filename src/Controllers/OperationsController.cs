b853bb581da97d728d95957d6dad4e0ea1c10d55
Uddip Mitra <uddipmitra@gmail.com>
2020-11-30 20:34:29 -0500

    Implement Functional API - like reloadconfig usng HttpOptions

---
+++ src/Controllers/OperationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public OperationsController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public ActionResult<bool> ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)config;
+                root.Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
